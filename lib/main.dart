02650bf4c2a574c8352ab8d3f5966742172d0995
JasonC01 <chungys02@gmail.com>
2023-08-24 22:48:26 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _APP());
+}
+
+class _APP extends StatelessWidget {
+  const _APP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
